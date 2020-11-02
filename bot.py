06e6a4c8de5016da053163bb3e336b4e2612c62b
Naaz Sibia <sibianjali@gmail.com>
2020-11-02 01:01:04 -0500

    Fix messages and add status

--- bot.py
+++ bot.py
@@ -16,36 +16,38 @@ client = discord.Client()
 class CustomClient(discord.Client):
     
     async def on_ready(self):
-        for guild in client.guilds:
+        for guild in self.guilds:
             if guild.name == GUILD:
                 break
 
         self.client = guild
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="you :)"))
 
     async def on_message(self, message):
-        help_msg = "Send a message with `*task seconds {task-name}` to set a timer"
+        help_msg = "Send a message with `*task minutes {task-name}` to set a timer"
 
         if message.content.startswith('*task'):
             msg = message.content.split()
-            if len(msg) < 3 or not msg[1].isdigit():
+            if len(msg) < 3 or not msg[1].replace(".", "").isdecimal():
                 await message.channel.send(help_msg)
             else: 
-                await message.channel.send('Timer set')
-                seconds = int(msg[1])
+                response = "Ok, {}! I will check on you in {} minutes :)".format(message.author.mention, msg[1])
+                await message.channel.send(response)
+                seconds = float(msg[1]) * 60
                 await asyncio.sleep(seconds)
-                await self.remind(message, msg[2])
+                await self.remind(message, msg[2], msg[1])
         
         if message.content.startswith('*help'):
             await message.channel.send(help_msg)
 
-    async def remind(self, message, task):
+    async def remind(self, message, task, mins):
         # check if user completed task:
         channel = self.client.get_channel(453417214591238166)
         curr_message = await channel.fetch_message(message.id)
         if curr_message.reactions and curr_message.reactions[0].emoji == '✅':
             await channel.send("Good job on completing the task, {}!".format(message.author.mention))
         else:
-            await channel.send("Hey {}, it's been 2 hours. Did you make progress on {}?".format(message.author.mention, task))
+            await channel.send("Hey {}, it's been {} minutes. Did you make progress on {}?".format(message.author.mention, mins, task))
 
 client = CustomClient()
 client.run(TOKEN)
