7cf7cf8f66911cb006a02a6dc108353b8959a129
Zain Kazmi <zkazmi786@hotmail.ca>
2020-11-21 01:33:17 -0500

    Add prediction function

--- MLP_classifier.py
+++ MLP_classifier.py
@@ -9,22 +9,31 @@ from sklearn.model_selection import train_test_split
 
 
 class NLPSpamDetector:
-    def execute(self):
+
+    def train_test(self):
         df = pd.read_csv('spam.csv', sep=',', encoding='latin-1')
         stop_words = set(stopwords.words('english'))
-        processed_text = []
-        target = []
+        self.processed_text = []
+        self.target = []
         for i in df.itertuples():
-            processed_text.append(i[2])
-            target.append(i[1])
-        tf = TfidfVectorizer()
-        t = tf.fit_transform(processed_text)
-        x_train,x_test,y_train,y_test = train_test_split(t,target,test_size = 0.3)
-        model = MLPClassifier(hidden_layer_sizes = (6,2), activation = 'tanh', learning_rate = 'adaptive', max_iter = 400)
-        model.fit(x_train, y_train)
-        print(model.predict(x_test))
-        print(model.score(x_test, y_test))
+            self.processed_text.append(i[2])
+            self.target.append(i[1])
+        self.tf = TfidfVectorizer()
+        t = self.tf.fit_transform(self.processed_text)
+        x_train,x_test,y_train,y_test = train_test_split(t,self.target,test_size = 0.3)
+        self.model = MLPClassifier(hidden_layer_sizes = (6,2), activation = 'tanh', learning_rate = 'adaptive', max_iter = 400)
+        self.model.fit(x_train, y_train)
+        self.model.predict(x_test)
+        print(self.model.score(x_test, y_test))
+
+    def predict(self, text):
+        self.processed_text.append(text)
+        t = self.tf.fit_transform(self.processed_text)
+        a = self.model.predict(t)
+        print(a[len(a)-1])
 
 if __name__=='__main__':
     a = NLPSpamDetector()
-    a.execute()
+    a.train_test()
+    a.predict("This is a user")
+
